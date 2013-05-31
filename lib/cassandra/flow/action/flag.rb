8fc829ec5ae5aa8645741762f2817b7ab59752d7
brainopia <brainopia@evilmartians.com>
2013-05-31 13:56:27 +0400

    Improve flag action to keep flag info between removals

--- lib/cassandra/flow/action/flag.rb
+++ lib/cassandra/flow/action/flag.rb
@@ -1,4 +1,3 @@
-# TODO: support removal
 class Cassandra::Flow::Action::Flag < Cassandra::Flow::Action
   attr_reader :flow, :name, :scope, :condition, :catalog
 
@@ -9,8 +8,53 @@ class Cassandra::Flow::Action::Flag < Cassandra::Flow::Action
   end
 
   def propagate(type, data)
-    flag! data if type == :insert
-    data
+    lock_name = 'flag:' + scope.map {|it| data[it] }.join('.')
+
+    lock(lock_name) do
+      record   = catalog.one scope: lock_name
+      previous = record[:data] if record
+      all      = record ? record[:all] : []
+
+      case type
+      when :insert
+        all << data
+        reflag = !previous || condition.call(data, previous)
+
+        if reflag
+          catalog.insert scope: lock_name, data: data, all: all
+          data[name] = true
+        else
+          catalog.insert scope: lock_name, data: previous, all: all
+        end
+
+        if reflag and previous
+          next_actions.propagate :remove, previous.merge(name => true)
+          next_actions.propagate :insert, previous
+        end
+      when :remove
+        if all.delete data
+          if data == previous
+            data[name] = true
+            new_data = all.sort {|a,b| condition.call(a,b) ? -1 : 1 }.first
+            if new_data
+              next_actions.propagate :remove, new_data
+              catalog.insert scope: lock_name, data: new_data, all: all
+
+              new_data[name] = true
+              next_actions.propagate :insert, new_data
+            else
+              catalog.remove scope: lock_name
+            end
+          else
+            catalog.insert scope: lock_name, data: previous, all: all
+          end
+        end
+      else
+        raise ArgumentError, "unsupported type: #{type}"
+      end
+
+      data
+    end
   end
 
   def setup!(flow)
@@ -22,31 +66,11 @@ class Cassandra::Flow::Action::Flag < Cassandra::Flow::Action
 
   def build_catalog
     keyspace = target.keyspace_base
-    table    = target.table + '_flag'
+    table    = target.table + '_flag_' + name.to_s
     Cassandra::Mapper.new keyspace, table do
-      key :scope
+      key  :scope
       type :data, :yaml
-    end
-  end
-
-  def flag!(data)
-    lock_name = 'flag:' + scope.map {|it| data[it] }.join('.')
-    reflag, previous = []
-
-    lock(lock_name) do
-      record   = catalog.one scope: lock_name
-      previous = record[:data] if record
-      reflag   = !previous || condition.call(data, previous)
-
-      if reflag
-        catalog.insert scope: lock_name, data: data
-        data[name] = true
-      end
-    end
-
-    if reflag and previous
-      next_actions.propagate :remove, previous.merge(name => true)
-      next_actions.propagate :insert, previous
+      type :all,  :yaml
     end
   end
 
--- spec/cassandra/flow/action/flag_spec.rb
+++ spec/cassandra/flow/action/flag_spec.rb
@@ -1,13 +1,15 @@
 require 'spec_helper'
 
 describe Cassandra::Flow::Action::Flag do
-  it 'should flag data' do
+  before do
     Cassandra::Flow
-      .new(facts)
+      .source(facts)
       .flag(:min, :project_id) {|new, old|
         new[:id] < old[:id]
       }.target(views)
+  end
 
+  it 'should insert with flag data' do
     facts.insert project_id: 72, id: 14
     facts.insert project_id: 72, id: 16
     facts.insert project_id: 72, id: 18
@@ -20,5 +22,30 @@ describe Cassandra::Flow::Action::Flag do
       { project_id: 72, id: 14, min: true },
       { project_id: 14, id: 14, min: true }
     ]
+  end
+
+  it 'should remove with flag data' do
+    facts.insert project_id: 72, id: 14
+    views.all.should == [{ project_id: 72, id: 14, min: true }]
+
+    facts.remove project_id: 72, id: 14
+    views.all.should == []
+  end
+
+  it 'should keep flag after removal' do
+    facts.insert project_id: 72, id: 14
+    facts.insert project_id: 72, id: 18
+    facts.insert project_id: 72, id: 10
+    facts.insert project_id: 72, id: 15
+
+    views.all.find {|it| it[:min] }.should include(id: 10)
+    facts.remove project_id: 72, id: 14
+    views.all.find {|it| it[:min] }.should include(id: 10)
+    facts.remove project_id: 72, id: 10
+    views.all.find {|it| it[:min] }.should include(id: 15)
+    facts.remove project_id: 72, id: 15
+    views.all.find {|it| it[:min] }.should include(id: 18)
+    facts.remove project_id: 72, id: 18
+    views.all.should == []
   end
 end
