9448e31793002828f39074b7aeb2d3e4dbf59ec0
brainopia <brainopia@evilmartians.com>
2013-06-05 13:32:02 +0400

    Ability to extend label

--- lib/cassandra/flow/actions/label.rb
+++ lib/cassandra/flow/actions/label.rb
@@ -2,6 +2,10 @@ class Cassandra::Flow::Action::Label < Cassandra::Flow::Action
   action!
 
   def setup!(suffix)
-    @suffix = suffix.to_s
+    if @suffix
+      @suffix += '_' + suffix.to_s
+    else
+      @suffix = suffix.to_s
+    end
   end
 end
